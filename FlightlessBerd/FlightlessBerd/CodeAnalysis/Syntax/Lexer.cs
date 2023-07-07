e75affe705ded6669dc210ec90a5a327b3d772f5
wackbyte <wackbyte@pm.me>
2023-07-07 00:43:47 -0400

    chore: format

--- FlightlessBerd/FlightlessBerd/CodeAnalysis/Syntax/Lexer.cs
+++ FlightlessBerd/FlightlessBerd/CodeAnalysis/Syntax/Lexer.cs
@@ -1,5 +1,7 @@
-﻿class Lexer {
-   static void Lex(string code) {
-     Console.WriteLine("Hello, yhvr.");
-   }
- }
+﻿class Lexer
+{
+    static void Lex(string code)
+    {
+        Console.WriteLine("Hello, yhvr.");
+    }
+}
